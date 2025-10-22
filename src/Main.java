ee16838bad1e9d317c6fa07ff0d061d57088c3b8
ibai <141GA05@ARRIAGAINFO.LOCAL>
2025-10-22 11:37:34 +0200

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static String cadena() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa la cadena:");
+        return sc.nextLine();
+    }
+    public static void main(String[] args) {
+
+    }
+}
